9d27d9ffee9f742aa574a85acb93a2ba9cf0f9e2
Eli Stevens <elis@doselab.com>
2010-10-04 15:25:15 -0700

    v0.0.2b1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 
 setup(
     name='couchable',
-    version='0.0.1b1',
+    version='0.0.2b1',
     author='Eli Stevens',
     author_email='wickedgrey@gmail.com',
     url='http://github.com/wickedgrey/couchable',
